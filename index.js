e47f4fe7397f986a650c71a66537d758e24fba99
unknown <bodkhesamadhan188@gmail.com>
2025-10-14 10:30:05 +0530

    second commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors({
     "http://localhost:3000",
     "http://localhost:5173",
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"],  
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 
@@ -51,4 +51,5 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app
