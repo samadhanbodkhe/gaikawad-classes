3c1e253c6e21f272558adf2eaac76773a8bb0c12
unknown <bodkhesamadhan188@gmail.com>
2025-10-14 12:58:51 +0530

    add http backend

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(cors({
     "http://localhost:3000",
     "http://localhost:5173",
     "http://gaikawad-classes.vercel.app",
-    "gaikawad-classes-admin-panal.vercel.app",
-    "gaikawad-classes-teacher-panal.vercel.app"
+    "https://gaikawad-classes-admin-panal.vercel.app",
+   "https://gaikawad-classes-teacher-panal.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
